96bf45862dd010a7ba95c88253685856449555d1
Pjotr <pdevos@roc-dev.com>
2021-01-11 08:57:30 +0100

    push

---
+++ h01/JavaScript.js
@@ -0,0 +1,14 @@
+console.log("Hello world!");
+
+for(var i = 0; i < 10; i++) {
+    console.log("De waarde van i is nu :")
+}
+
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalwielen: 4,
+    kleur: "Rood"
+}
+
+console.log(mijnauto.kleur)
