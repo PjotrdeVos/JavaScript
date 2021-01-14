a05401bf2cdba47ca783dd56298e5b610a8ba2d4
Pjotr <pdevos@roc-dev.com>
2021-01-14 10:49:06 +0100

    Push

---
+++ h02/JavaScript.js
@@ -0,0 +1,13 @@
+
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel"
+document.getElementById("subkop").innerText = "Dit is de subkop"
+document.getElementById("aap1").src = "images/Plaatje1.jpg"
+document.getElementById("aap2").src = "images/Plaatje2.png"
+
+document.getElementById("bl1").style.backgroundColor = "blue";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+document.getElementById("bl2").style.marginTop = "25px";
+document.getElementById("bl3").style.marginTop = "50px";
