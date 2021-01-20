d7a447e59573ea3e3050516bf5b1f8e60103d9df
Pjotr <pdevos@roc-dev.com>
2021-01-20 09:49:58 +0100

    Push

--- h01/JavaScript.js
+++ h01/JavaScript.js
@@ -1,14 +1,14 @@
 console.log("Hello world!");
 
 for(var i = 0; i < 10; i++) {
-    console.log("De waarde van i is nu :")
+    console.log("De waarde van i is nu : " + i)
 }
 
 var mijnauto = {
     merk: "Ford",
     type: "Mondeo",
     aantalwielen: 4,
-    kleur: "Geel"
+    kleur: "Groen"
 }
 
 console.log(mijnauto.kleur)
---
+++ h04/JavaScript.js
@@ -0,0 +1,17 @@
+var MijnAuto = {
+    kleur: "Geel",
+    merk: "Ford",
+
+
+    toet: function () {
+        console.log("Toet!")
+    }
+}
+MijnAuto.toet();
+
+
+
+
+var Plaatjes =  [1,2,3,4,5,6,7,8,9];
+var SlideHolder = document.getElementById("SlideHolder");
+SlideHolder.style.backgroundImage = "url('Plaatjes/Aap1.jpg')";
