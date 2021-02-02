1ae952a1fe1cc5fb883d4c825a31e8e6f12b3cfe
Pjotr <pdevos@roc-dev.com>
2021-02-02 09:58:58 +0100

    Push

--- h04/JavaScript.js
+++ h04/JavaScript.js
@@ -14,4 +14,23 @@ MijnAuto.toet();
 
 var Plaatjes =  [1,2,3,4,5,6,7,8,9];
 var SlideHolder = document.getElementById("SlideHolder");
+var teller = 0;
 SlideHolder.style.backgroundImage = "url('Plaatjes/Aap1.jpg')";
+
+SlideHolder.addEventListener("click", function () {
+    SlideHolder.style.backgroundImage = "url('Plaatjes/Aap" +getAap() + ".jpg')";
+});
+
+function getAap() {
+    if (teller >= Plaatjes.length) {
+        teller = 1;
+    } else {
+        teller++;
+    }
+    console.log(teller)
+    return(teller)
+}
+
+
+
+
---
+++ h04/crook/JavaScript.js
@@ -0,0 +1,50 @@
+var Ogen = [1,2,3];
+var Neus = [1,2,3];
+var Mond = [1,2,3];
+
+var crook1 = document.getElementById("crook1");
+crook1.style.backgroundImage = "url('crook/Ogen/Ogen1.jpg')";
+var crook2 = document.getElementById("crook2");
+crook2.style.backgroundImage = "url('crook/Neus/Neus1.jpg')";
+var crook3 = document.getElementById("crook3");
+crook3.style.backgroundImage = "url('crook/Mond/Mond1.jpg')";
+
+crook1.addEventListener("click", function() {
+    crook1.style.backgroundImage = "url('crook/Ogen/Ogen" + getCrookOgen() + ".png')";
+});
+crook2.addEventListener("click", function() {
+    crook2.style.backgroundImage = "url('crook/Neus/Neus" + getCrookNeus() + ".png')";
+});
+crook3.addEventListener("click", function() {
+    crook3.style.backgroundImage = "url('crook/Mond/Mond" + getCrookMond() + ".png')";
+});
+
+function getCrookOgen() {
+    if (teller >= Ogen.length) {
+        teller = 1;
+    } else {
+        teller++;
+    }
+    console.log(teller)
+    return(teller)
+}
+
+function getCrookNeus() {
+    if (teller >= Neus.length) {
+        teller = 1;
+    } else {
+        teller++;
+    }
+    console.log(teller)
+    return(teller)
+}
+
+function getCrookMond() {
+    if (teller >= Mond.length) {
+        teller = 1;
+    } else {
+        teller++;
+    }
+    console.log(teller)
+    return(teller)
+}
