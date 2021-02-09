72df3729c88f1d5c19f276903c4577e4967018b8
Pjotr <pdevos@roc-dev.com>
2021-02-09 11:18:43 +0100

    Push

--- h04/crook/JavaScript.js
+++ h04/crook/JavaScript.js
@@ -1,13 +1,14 @@
 var Ogen = [1,2,3];
 var Neus = [1,2,3];
 var Mond = [1,2,3];
+var teller = 0;
 
 var crook1 = document.getElementById("crook1");
-crook1.style.backgroundImage = "url('crook/Ogen/Ogen1.jpg')";
+crook1.style.backgroundImage = "url('crook/Ogen/Ogen1.png')";
 var crook2 = document.getElementById("crook2");
-crook2.style.backgroundImage = "url('crook/Neus/Neus1.jpg')";
+crook2.style.backgroundImage = "url('crook/Neus/Neus1.png')";
 var crook3 = document.getElementById("crook3");
-crook3.style.backgroundImage = "url('crook/Mond/Mond1.jpg')";
+crook3.style.backgroundImage = "url('crook/Mond/Mond1.png')";
 
 crook1.addEventListener("click", function() {
     crook1.style.backgroundImage = "url('crook/Ogen/Ogen" + getCrookOgen() + ".png')";
